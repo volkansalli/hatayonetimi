6991b85f5720569294a924c4a2c9524bc5d9bb9a
volkan salli <volkan_salli@hotmail.com>
2021-11-09 22:35:43 +0300

    ok

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+            Console.WriteLine("bir tam sayi giriniz");
+            int x=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x);
+            }
+            catch(Exception ex){
+                Console.WriteLine("hatali giris "+ ex.Message.ToString());
+            }
+            finally{
+                Console.WriteLine("dogru giris yapildi");
+            }
+           
+            try {
+                Console.WriteLine("lütfen console da NULL dan farklı bir giris yapınız");
+                int y=int.Parse(Console.ReadLine());
+
+            }
+            catch (ArgumentException ex){
+                Console.WriteLine("hatali giris lütfen girdi yapmadan enter e basmayın");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex){
+                Console.WriteLine(ex);
+            }
+            
+        }
+    }
+}
